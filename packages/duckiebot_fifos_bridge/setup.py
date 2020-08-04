43ec7f2ec7abb8a098b0913d5ef0505f471e0000
liampaull <paulll@iro.umontreal.ca>
2020-08-04 16:13:48 -0400

    - to _

--- packages/duckiebot_fifos_bridge/setup.py
+++ packages/duckiebot_fifos_bridge/setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
     packages=[
-        'duckiebot-fifos-bridge'
+        'duckiebot_fifos_bridge'
     ],
     package_dir={'': 'include'},
 )
